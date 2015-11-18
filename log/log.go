73b4592636994110bfa9506fe7ae339c880feebc
Mark Hapner <mark.hapner@resilient-networks.com>
2015-11-17 19:11:54 -0800

    fixed initialization order issue

--- log/log.go
+++ log/log.go
@@ -1,7 +1,11 @@
 /*
 Package log provides a configured instance of a log package logger that is shared within an executable.
-Typically the executable will provide -log, -logprefix and -logflg command line switches containing respectively the log file name, log prefix and log flag values.
+Typically the executable will provide -log, -logprefix and -logflg command line switches containing respectively
+the log file name, log prefix and log flag values.
 The executable's init will parse these command line flags and then configure this log instance with them.
+If Config is not called, the default is to log to stderr with no prefix and no flag.
+
+Due to initialization order issues, this logger cannot be used in init() functions.
 
 See standard go log package for more info.
 */
@@ -12,7 +16,103 @@ import (
 	"os"
 )
 
-var logger *golog.Logger
+type (
+	LoggerT struct {
+		logger *golog.Logger
+	}
+)
+
+var logger = new(LoggerT)
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Fatal(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Fatal(v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Fatalf(format string, v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Fatalf(format, v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Fatalln(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Fatalln(v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Panic(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Panic(v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Panicf(format string, v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Panicf(format, v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Panicln(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Panicln(v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Print(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Print(v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Printf(format string, v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Printf(format, v)
+}
+
+/*
+Fatal delegates to the shared golang logger
+*/
+func (l *LoggerT) Println(v ...interface{}) {
+	if l.logger == nil {
+		Config("", "", 0)
+	}
+	l.logger.Println(v)
+}
 
 /*
 Config initializes the shared log instance. It should be called from an executable's init function. If it is not called, a default log instance that logs to os.Stderr is created.
@@ -32,7 +132,7 @@ func Config(logname, logprefix string, logflg int) {
 		logFile = os.Stderr
 	}
 
-	logger = golog.New(logFile, logprefix, logflg)
+	logger.logger = golog.New(logFile, logprefix, logflg)
 
 	if openErr != nil {
 		logger.Printf("Logging to stderr because opening log file with Name: %v failed with Error: %v\n", logname, openErr)
@@ -42,9 +142,6 @@ func Config(logname, logprefix string, logflg int) {
 /*
 Logger returns the shared logger
 */
-func Logger() *golog.Logger {
-	if logger == nil {
-		Config("", "", 0)
-	}
+func Logger() *LoggerT {
 	return logger
 }
